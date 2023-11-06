be25a0dd4bf0439c57b6bf2c51f27bf75d83c967
Larissa Paixao <larissapaixaolsp@gmail.com>
2023-11-06 10:43:38 -0300

    Initial commit

---
+++ src/Pessoa.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class Pessoa {
+    // Atributos
+    private String nome;
+    private LocalDate dataNascimento;
+    private String cpf;
+    private double altura;
+    private double peso;
+    
+}
